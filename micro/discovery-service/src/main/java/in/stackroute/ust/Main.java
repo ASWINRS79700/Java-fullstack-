60878032b8209d45049f881c37396852e079df05
ASWINRS79700 <103929687+ASWINRS79700@users.noreply.github.com>
2023-06-07 14:50:18 +0530

    Add files via upload

---
+++ micro/discovery-service/src/main/java/in/stackroute/ust/Main.java
@@ -0,0 +1,18 @@
+package in.stackroute.ust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableEurekaServer
+@ComponentScan(basePackages = "in.stackroute.ust")
+
+public class Main {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class,args);
+    }
+}
